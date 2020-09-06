be8c13d8b898aefc87640b39f16011a949d04e1b
filipton <37213766+filipton@users.noreply.github.com>
2020-09-06 14:42:33 +0200

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = .1f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+
+        transform.position += new Vector3(horizontal, vertical, 0);
+    }
+}
